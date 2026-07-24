6a9269443aa3de41115494be84c4f2d3bc2dab93
Rocky-Alex <rocky.alex@example.com>
2026-07-24 20:19:41 +0400

    feat: track setup binaries with Git LFS, add keyboard and sound checking assets

---
+++ dist/version.js
@@ -0,0 +1,9 @@
+// ============================================================================
+//                       BC ELITE QC - VERSION CONFIGURATION
+// ============================================================================
+// Change the string below to update the version shown in the software UI.
+// You can set it to a standard version number (e.g. "1.0.6") or custom text
+// (e.g. "v1.0.6-Beta", "Enterprise Edition", etc.).
+// If left empty or undefined, it will fallback to the Tauri package version.
+// ============================================================================
+window.APP_VERSION = "1.5";
